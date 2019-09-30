629bad0a1af370a0a02e4c9fd2ff4b3df85c5657
fliepeltje <donatas@rasiukevicius.dev>
2019-09-30 08:52:07 +0200

    feature(log): prettified log

--- src/log.rs
+++ src/log.rs
@@ -1,12 +1,13 @@
 use crate::api::client_from_env;
 use chrono::{Datelike, Local, NaiveDate, Weekday};
+use colored::*;
 use simplicate::hours::Hours;
 
 #[derive(Clone)]
 pub struct LoggedHour {
     pub description: String,
     pub time: f64,
-    pub updated_at: Option<String>,
+    pub updated_at: String,
 }
 
 pub struct HoursLog {
@@ -35,33 +36,43 @@ impl HoursLog {
         hours.sort_by(|a, b| a.start_date.cmp(&b.start_date));
         let mut header = "unknown".to_string();
         for h in hours {
-            let lh = LoggedHour {
-                description: match h.project {
-                    Some(proj) => {
-                        let name = proj.name.unwrap_or("Unnamed project".to_string());
-                        let note = h.note.unwrap_or("".to_string());
-                        format!("{}: {}", name, note)
-                    }
-                    None => "Unknown project".to_string(),
-                },
-                time: h.hours,
-                updated_at: h.created_at,
-            };
             match &h.start_date {
                 Some(x) => {
                     let h = x.split(" ").next().unwrap_or("unknown");
                     let h = h.to_string();
                     if h != header {
                         header = h;
-                        println!("{}", header);
+                        println!("{}", header.bold().green());
                     };
                 }
                 None => (),
             };
+            let lh = LoggedHour {
+                description: match h.project {
+                    Some(proj) => {
+                        let name = proj.name.unwrap_or("Unnamed project".to_string());
+                        let note = h.note.unwrap_or("".to_string());
+                        format!("{}: {}", name.bright_red(), note.yellow())
+                    }
+                    None => "Unknown project".to_string(),
+                },
+                time: h.hours,
+                updated_at: match h.start_date {
+                    Some(s) => {
+                        let mut elements = s.split(" ");
+                        elements.next();
+                        elements
+                            .next()
+                            .unwrap_or(&"no time".to_string())
+                            .to_string()
+                    }
+                    _ => "unknown".to_string(),
+                },
+            };
             println!(
                 "    {}\t\t{}\t{}",
-                lh.updated_at.unwrap_or("".to_string()),
-                lh.time,
+                lh.updated_at.italic().magenta(),
+                lh.time.to_string().bold().blue(),
                 lh.description,
             );
         }
